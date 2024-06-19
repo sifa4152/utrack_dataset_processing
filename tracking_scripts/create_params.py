5f2a6ba47ad6520719c9df9fb4405d6fc9977f19
sifa4152 <118833344+sifa4152@users.noreply.github.com>
2024-06-19 12:17:04 +0200

    Update create_params.py

--- tracking_scripts/create_params.py
+++ tracking_scripts/create_params.py
@@ -6,10 +6,6 @@ import sys
 import settings
 from utils import params_creator
 
-# sys.path.insert(1,'/p/projects/open/simon/bgwater/GCEW/final_report/NL_BE_case_study/2024_05_08_NL_BE_runs')
-# from importlib import reload # reload 
-# reload(settings)
-
 
 if __name__ == '__main__':
     files = glob.glob(os.path.join(settings.PATH_TARGET_ZONES, 'target_cells_*.csv')) # target cell coordinates (lat,lon)
