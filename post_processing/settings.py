a9051b3e176084b6aea036e5c3fb9f5131d68faa
sifa4152 <118833344+sifa4152@users.noreply.github.com>
2024-08-20 11:12:17 +0200

    Update settings.py

--- post_processing/settings.py
+++ post_processing/settings.py
@@ -8,28 +8,29 @@ import os
 # SETTINGS 
 PERCENTILE = 99 # allocation of moisture to watersheds
 OUTPUT_FORMAT = 'geojson' # 'geojson' or 'netcdf'
+MAX_WORKERS = 4 # number of workers for parallel processing
 
 
 # PARAMETERS
-PARAMS = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'GCEW', 'final_report', 'global_basin_utrack_runs', '2024_08_13_basins_selected', 'params.csv')
+PARAMS = os.path.join(WRKDIR, 'params.csv')
 
 
 # INPUTS
-LSM_PATH = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'data_repo', 'era5_reanalysis', 'era5_lsm', 'era5_lsm_remap_utrack.nc')
-PATH_TARGET_ZONES = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'data_repo', 'study_regions', 'countries', 'CNTR_OC_mask_EUROSTAT_GOaS', 'CNTR_OC_mask_EUROSTAT_GOaS.nc')
-GRID_PATH = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'data_repo', 'era5_reanalysis', 'era5_grid_area', 'era5_grid_area_m2_utrack.nc')
-PRECIP_PATH = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'data_repo', 'era5_reanalysis', 'era5_precipitation', 'era5_monthly_tp_1979_2021_mm_month_remap.nc')
-EVAP_PATH = os.path.join('/p','projects','open','simon','bgwater','data_repo','era5_reanalysis','era5_evaporation','era5_monthly_evap_1979_2021_mm_month_remap.nc') 
+LSM_PATH = os.path.join('data', 'era5_reanalysis', 'era5_lsm', 'era5_lsm_remap_utrack.nc')
+PATH_TARGET_ZONES = os.path.join('data', 'study_regions', 'countries', 'CNTR_OC_mask_EUROSTAT_GOaS', 'CNTR_OC_mask_EUROSTAT_GOaS.nc')
+GRID_PATH = os.path.join('data', 'era5_reanalysis', 'era5_grid_area', 'era5_grid_area_m2_utrack.nc')
+PRECIP_PATH = os.path.join('data', 'era5_reanalysis', 'era5_precipitation', 'era5_monthly_tp_1979_2021_mm_month_remap.nc')
+EVAP_PATH = os.path.join('data','era5_reanalysis','era5_evaporation','era5_monthly_evap_1979_2021_mm_month_remap.nc') 
 
 
 # FOOTPRINTS
-PATH_MOISTURE_FOOTPRINTS = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'GCEW', 'final_report', 'global_basin_utrack_runs', '2024_08_13_basins_selected','moisture_footprints')
+PATH_MOISTURE_FOOTPRINTS = os.path.join('output_dir','moisture_footprints')
 TOTAL_FORWARD = os.path.join(PATH_MOISTURE_FOOTPRINTS, 'forward_complete')
 TOTAL_BACKWARD = os.path.join(PATH_MOISTURE_FOOTPRINTS, 'backward_complete')
 
 
 # PROCESS OUTPUTS
-OUT = os.path.join('/p', 'projects', 'open', 'simon', 'bgwater', 'GCEW', 'final_report', 'global_basin_utrack_runs', '2024_08_13_basins_selected', 'watershed_processing')
+OUT = os.path.join('output_dir', 'watershed_processing')
 
 if OUTPUT_FORMAT == 'geojson':
     if not os.path.exists(os.path.join(OUT, 'esheds_geojson')):
